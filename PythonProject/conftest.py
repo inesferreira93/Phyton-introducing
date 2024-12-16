daa2d6daaf383367f5a5eb9633dbb587d5c88b91
Ines Ferreira <ines.ferreira@kununu.com>
2024-12-16 16:58:44 +0000

    fixing pipeline

--- PythonProject/conftest.py
+++ PythonProject/conftest.py
@@ -1,6 +1,5 @@
-import os
 import pytest
-from utils.browser import initialize_browser, capture_screenshot
+from PythonProject.utils.browser import initialize_browser, capture_screenshot
 
 @pytest.fixture
 def browser():
@@ -14,10 +13,6 @@ def pytest_runtest_makereport(item, call):
     rep = outcome.get_result()
 
     if rep.when == "call" and rep.failed:
-        mode = "a" if os.path.exists("screenshot_failures.log") else "w"
-        with open("screenshot_failures.log", mode) as f:
-            if "browser" in item.funcargs:
-                driver = item.funcargs["browser"]
-                screenshot_path = f"screenshots/{item.nodeid.replace('::', '_')}.png"
-                capture_screenshot(driver, item.nodeid.replace("::", "_"))
-                f.write(f"Screenshot captured: {screenshot_path}\n")
+        if "browser" in item.funcargs:
+            driver = item.funcargs["browser"]
+            capture_screenshot(driver, item.nodeid.replace("::", "_"))
